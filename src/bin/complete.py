5a79edb8c6fb95d518abb390d1a2ce2cdaa8d429
刘国鸿 <84012452+ReminiscencertheForgotten@users.noreply.github.com>
2022-07-06 17:29:55 +0800

    Delete src/bin directory

--- src/bin/complete.py
+++
@@ -1,8 +0,0 @@
-fp = open(r'inst.txt', 'r')
-line_number = len(fp.readlines())
-fp.close()
-fp = open(r'inst.txt', 'a')
-while line_number < 512:
-    fp.write('00000000\n')
-    line_number += 1
-fp.close()
--- src/bin/dumpfile.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\Zs_Byqx2020\Desktop\CPU_pipeline\src\bin
-java -jar Mars4_5.jar dump 0x10010000-0x10010800 HexText data.txt test.asm
-java -jar Mars4_5.jar dump 0x00400000-0x00400800 HexText inst.txt test.asm
-python complete.py
